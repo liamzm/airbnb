20f4bb3ef2d414ccf74439bb4ffcb62bb3862016
liamzm <liamzm@gmail.com>
2018-06-25 15:48:14 +0800

    fixed migrations

--- db/migrate/20180611064039_rename_users_id_to_user_id_in_listings.rb
+++
@@ -1,6 +0,0 @@
-class RenameUsersIdToUserIdInListings < ActiveRecord::Migration[5.2]
-  def change
-  	rename_column :properties, :users_id, :user_id
-  	remove_column :users, :property_id
-  end
-end
