e792a4c9e308d2eaa965dd5fdc10e451b8796e67
liamzm <liamzm@gmail.com>
2018-06-26 15:08:42 +0800

    google auth fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_111958) do
+ActiveRecord::Schema.define(version: 2018_06_25_101720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2018_06_20_111958) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "hosts", force: :cascade do |t|
+  end
+
   create_table "properties", force: :cascade do |t|
     t.string "address", null: false
     t.string "city", null: false
@@ -59,6 +62,9 @@ ActiveRecord::Schema.define(version: 2018_06_20_111958) do
     t.datetime "updated_at", null: false
     t.index ["property_id"], name: "index_reservations_tables_on_property_id"
     t.index ["user_id"], name: "index_reservations_tables_on_user_id"
+  end
+
+  create_table "tables", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
